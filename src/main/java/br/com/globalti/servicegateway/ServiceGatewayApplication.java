787fa6b81add0e1b1e804a2274c105cc6f574391
Kleber Nunes <kleber_nunes99@hotmail.com>
2018-09-22 22:54:05 -0300

    configurações gateway

--- src/main/java/br/com/globalti/servicegateway/ServiceGatewayApplication.java
+++ src/main/java/br/com/globalti/servicegateway/ServiceGatewayApplication.java
@@ -1,11 +1,39 @@
 package br.com.globalti.servicegateway;
 
+import br.com.globalti.servicegateway.filters.ErrorFilter;
+import br.com.globalti.servicegateway.filters.PostFilter;
+import br.com.globalti.servicegateway.filters.PreFilter;
+import br.com.globalti.servicegateway.filters.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class ServiceGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceGatewayApplication.class, args);
 	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+
 }
---
+++ src/main/java/br/com/globalti/servicegateway/filters/ErrorFilter.java
@@ -0,0 +1,26 @@
+package br.com.globalti.servicegateway.filters;
+
+import com.netflix.zuul.ZuulFilter;
+
+public class ErrorFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "error";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        return null;
+    }
+}
